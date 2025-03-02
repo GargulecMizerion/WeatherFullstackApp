5029d666360e5e914df1f5ca311a0f18f0a4bbc3
GargulecMizerion <xmaszekx@gmail.com>
2025-03-02 17:48:25 +0100

    WelcomeScreen - rozpoczecie stylowania

--- frontend/app/(screens)/WelcomeScreen.js
+++ frontend/app/(screens)/WelcomeScreen.js
@@ -1,12 +1,32 @@
-import {Button} from "@react-navigation/elements";
-import {View} from "react-native";
-import {useNavigation} from "@react-navigation/native";
+import { View, Image, Button, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const WelcomeScreen = () => {
     const navigation = useNavigation();
-    return <View className="flex-1 bg-amber-600">
-        <Button className={"bg-black"} onPress={() => navigation.navigate("Home") }>Przejdz do ...</Button>
-    </View>
-}
+
+    return (
+        <SafeAreaView className="h-full">
+            <View className="bg-primary p-10 justify-center items-center h-full">
+                <Text className="text-5xl text-sea font-bold top-20 z-10">
+                    Typhoon
+                </Text>
+                <Image
+                    source={require("../../assets/images/logo.png")}
+                    className="w-4/5 m-0 h-1/2"
+                    resizeMode={"contain"}
+                />
+
+                <Button
+                    title="Welcome Backa!"
+                    onPress={() => navigation.navigate("Home")}
+                    className={"rounded-lg bg-black w-full"}
+                    color={"#026463"}
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
 
 export default WelcomeScreen;
+;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FDFAE9",
+        sea: "#026463",
+      }
+    },
   },
   plugins: [],
   presets: [require("nativewind/preset")],
