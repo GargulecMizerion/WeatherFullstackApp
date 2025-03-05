11b35c69fd1875f07eff420b20ce65563f4fe8d3
GargulecMizerion <xmaszekx@gmail.com>
2025-03-05 16:57:59 +0100

    Dzialajace wyszukiwanie

--- frontend/app/(screens)/HomeScreen.js
+++ frontend/app/(screens)/HomeScreen.js
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {FlatList, View, Text, Dimensions, ScrollView, TextInput} from "react-native";
+import {View, Text, ScrollView, TextInput, Platform} from "react-native";
 import {LineChart} from "react-native-chart-kit";
 import {SafeAreaView} from "react-native-safe-area-context";
+import {Button} from "@rneui/base";
+
 
 const HomeScreen = () => {
     const [data, setData] = useState([]);
     const [labels, setLabels] = useState([]);
     const [temperature, setTemperature] = useState([]);
     const [cityName, setCityName] = useState("");
+    const [inputFocus, setInputFocus] = useState("Podaj nazwę miasta...");
+
+    const BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:8080?city=': 'http://localhost:8080?city=';
 
     const parseDate = (dt) => {
         const date = new Date(dt*1000);
@@ -18,86 +23,131 @@ const HomeScreen = () => {
         return formattedDateTime;
     }
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://10.0.2.2:8080/?city=Warszawa');
-                setData(response.data || []); // Zapobieganie błędom, jeśli `response.data` jest `null`
+    const fetchData = async (city) => {
+        try {
+            const response = await axios.get(BASE_URL + city);
+            setData(response.data || []); // Zapobieganie błędom, jeśli `response.data` jest `null`
+            setLabels(response.data.map((item) => parseDate(item.dt)));
+            setTemperature(response.data.map((item) => item.main.temp));
+        } catch (error) {
+            console.error("Błąd pobierania danych:", error);
+        }
+    };
 
-               setLabels(response.data.map((item) => parseDate(item.dt)));
-               setTemperature(response.data.map((item) => item.main.temp));
-            } catch (error) {
-                console.error("Błąd pobierania danych:", error);
-            }
-        };
-
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //
+    //     fetchData();
+    // }, []);
 
     const getToday = () => {
         const today = new Date();
-
         const dayName = today.toLocaleDateString('pl-PL', { weekday: 'long' });
-
         return dayName.charAt(0).toUpperCase() + dayName.slice(1);
     }
 
     const getDate = () => {
         const today = new Date();
-
         return today.toLocaleDateString('pl-PL');
+    }
+
+    const handleSearch = (city) => {
+        setCityName(city);
+        fetchData(city);
     }
 
     return (
         <SafeAreaView className={"bg-primary h-full"}>
-        <View className={"mt-5 ml-2"}>
+        <View className={"mt-5"}>
             <Text className={"text-center text-4xl "}> Dziś jest <Text className={"text-sea font-bold"}>{getToday()}</Text>,</Text>
             <Text className={"text-center text-3xl"}> {getDate()}</Text>
-            <TextInput  color={"white"} className={"bg-sea mr-2 p-2"} borderRadius={5}/>
-            {data.length === 0 ? (
-                <Text className={"text-4xl"}>Ładowanie danych...</Text>
+            <View className={"flex-row gap-2 m-2 items-center"} style={{ height: 50 }}>
+                <TextInput
+                    color={"white"}
+                    className={"bg-sea p-2"}
+                    borderRadius={100}
+                    style={{
+                        padding: 10,
+                        fontSize: 16,
+                        flexGrow: 1,
+                        height: 50
+                    }}
+                    placeholder={inputFocus}
+                    textAlign={"center"}
+                    onFocus={() => setInputFocus("")}
+                    onEndEditing={() => setInputFocus("Podaj nazwę miasta...")}
+                    onChangeText={setCityName}
+                />
+                <Button
+                    icon={{
+                        name: 'search',
+                        type: 'font-awesome',
+                        size: 15,
+                        color: 'white',
+                    }}
+                    buttonStyle={{
+                        backgroundColor: 'rgba(90, 154, 230, 1)',
+                        borderColor: 'transparent',
+                        borderWidth: 0,
+                        borderRadius: 25, // Połowa szerokości/wysokości dla idealnego koła
+                        width: 50,
+                        height: 50,
+                        padding: 0, // Usuń nadmiarowe paddingi, aby zachować idealny kształt
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                    containerStyle={{
+                        width: 50,
+                        height: 50,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        alignSelf: "center",
+                    }}
+                    onPress={() => fetchData(cityName)}
+                />
+            </View>
+            {cityName === "" ? (
+                <Text className={"text-4xl"}>Wyszukaj swoje miasto</Text>
             ) : (
-                <ScrollView horizontal={true}>
-
-                    <LineChart
-                        data={{
-                            labels: labels,
-                            datasets: [
-                                {
-                                    data: temperature,
+                data.length === 0 ? null : (
+                    <ScrollView horizontal={true}>
+                        <LineChart
+                            data={{
+                                labels: labels,
+                                datasets: [
+                                    {
+                                        data: temperature,
+                                    }
+                                ]
+                            }}
+                            width={Math.max(800, labels.length * 150)} // from react-native
+                            height={220}
+                            yAxisSuffix="°C"
+                            yAxisInterval={1} // optional, defaults to 1
+                            chartConfig={{
+                                backgroundColor: "#e26a00",
+                                backgroundGradientFrom: "#fb8c00",
+                                backgroundGradientTo: "#ffa726",
+                                decimalPlaces: 1, // optional, defaults to 2dp
+                                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16
+                                },
+                                propsForDots: {
+                                    r: "6",
+                                    strokeWidth: "2",
+                                    stroke: "#ffa726"
                                 }
-                            ]
-                        }}
-                        width={Math.max(800, labels.length * 150)} // from react-native
-                        height={220}
-
-                        yAxisSuffix="°C"
-                        yAxisInterval={1} // optional, defaults to 1
-                        chartConfig={{
-                            backgroundColor: "#e26a00",
-                            backgroundGradientFrom: "#fb8c00",
-                            backgroundGradientTo: "#ffa726",
-                            decimalPlaces: 1, // optional, defaults to 2dp
-                            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                            style: {
+                            }}
+                            bezier
+                            style={{
+                                marginVertical: 8,
+                                marginRight: 5,
                                 borderRadius: 16
-                            },
-                            propsForDots: {
-                                r: "6",
-                                strokeWidth: "2",
-                                stroke: "#ffa726"
-                            }
-                        }}
-                        bezier
-                        style={{
-                            marginVertical: 8,
-
-                            borderRadius: 16
-                        }}
-                    />
-
-                </ScrollView>
+                            }}
+                        />
+                    </ScrollView>
+                )
             )}
         </View>
         </SafeAreaView>
